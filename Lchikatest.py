c9d09dbd822ccaebc24617012db6c4f4b58dfb5c
Your Name <you@example.com>
2024-03-04 17:12:50 +0900

    yattane

--- Lchikatest.py
+++ Lchikatest.py
@@ -7,11 +7,11 @@ chip = gpiod.Chip('gpiochip4', gpiod.Chip.OPEN_BY_NAME)
 line = chip.get_line(PIN_NO)
 line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
 
-# GPIOライン25をHIGHに設定
-line.set_value(1)
-
-# 1秒待機
-time.sleep(1)
-
-# GPIOライン25をLOWに設定
-line.set_value(0)
+try:
+    while True:
+        line.set_value(1)
+        time.sleep(1)
+        line.set_value(0)
+        time.sleep(1)
+finally:
+    line.release()
