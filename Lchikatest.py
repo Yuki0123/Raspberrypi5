7f7b187ea2082e608b9f65839b3cc9822fe62e99
Your Name <you@example.com>
2024-03-04 16:32:59 +0900

    renew

--- Lchikatest.py
+++ Lchikatest.py
@@ -1,25 +1,16 @@
 import gpiod
 
 # GPIO コントローラーチップ 0 を取得
-chip = gpiod.Chip('0')
+chip = gpiod.Chip(0)
 
-# GPIO ライン 25 を取得
+# GPIO ライン 26 を取得
 line = chip.get_line(26)
 
-# GPIO ライン 25 を出力として設定
-#line.request(gpiod.LINE_REQUEST_DIRECTION_OUT)
+# GPIO ライン 26 を出力として設定
 line.request(gpiod.chip.DIRECTION_OUT)
 
-# 無限ループで点滅
-while True:
-  # GPIO ライン 25 を HIGH に設定
-  line.set_value(1)
+# GPIO ライン 26 を HIGH に設定
+line.set_value(1)
 
-  # 1 秒間待つ
-  time.sleep(1)
-
-  # GPIO ライン 25 を LOW に設定
-  line.set_value(0)
-
-  # 1 秒間待つ
-  time.sleep(1)
+# GPIO ライン 26 を LOW に設定
+line.set_value(0)
