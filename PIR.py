3cd0bf65a05cd43350feb0c47f90a5e287114701
Your Name <you@example.com>
2024-03-06 22:35:03 +0900

    a

--- PIR.py
+++ PIR.py
@@ -1,62 +1,59 @@
-import RPi.GPIO as GPIO
+import gpiod
 import time
+from gpiozero import PWMOutputDevice
+import random
 
-rgbPins = {'Red':18, 'Green':27, 'Blue':22}
-pirPin = 17    # the pir connect to pin17
+# GPIOピンの定義
+LED_PIN_RED = 22
+LED_PIN_GREEN = 18
+LED_PIN_BLUE = 27
+pirPin=17
+
+# GPIOチップの初期化
+chip = gpiod.Chip('gpiochip4')  # 使用するgpiochipが異なる場合は変更
+
+# GPIOラインのリクエスト (出力モードで)
+pir_line=chip.get_line(pirPin)
+
+pir_line.request(consumer="PIR", type=gpiod.LINE_REQ_DIR_IN)
 
 def setup():
-    global p_R, p_G, p_B
-    GPIO.setmode(GPIO.BCM)          # Set the GPIO modes to BCM Numbering
-    GPIO.setup(pirPin, GPIO.IN)    # Set pirPin to input
-    # Set all LedPin's mode to output and initial level to High(3.3v)
-    for i in rgbPins:
-        GPIO.setup(rgbPins[i], GPIO.OUT, initial=GPIO.HIGH)
+    global led_red_pwm,led_green_pwm,led_blue_pwm
 
-    # Set all led as pwm channel and frequece to 2KHz
-    p_R = GPIO.PWM(rgbPins['Red'], 2000)
-    p_G = GPIO.PWM(rgbPins['Green'], 2000)
-    p_B = GPIO.PWM(rgbPins['Blue'], 2000)
+    # 赤色LED用PWMデバイスの生成
+    led_red_pwm = PWMOutputDevice(LED_PIN_RED, frequency=2000)
+    led_green_pwm = PWMOutputDevice(LED_PIN_GREEN, frequency=2000)
+    led_blue_pwm = PWMOutputDevice(LED_PIN_BLUE, frequency=2000)
 
-    # Set all begin with value 0
-    p_R.start(0)
-    p_G.start(0)
-    p_B.start(0)
+    led_red_pwm.value=0
+    led_green_pwm.value=0
+    led_blue_pwm.value=0
+    
 
-# Define a MAP function for mapping values.  Like from 0~255 to 0~100
-def MAP(x, in_min, in_max, out_min, out_max):
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-
-# Define a function to set up colors
-def setColor(color):
-# configures the three LEDs' luminance with the inputted color value .
-    # Devide colors from 'color' veriable
-    R_val = (color & 0xFF0000) >> 16
-    G_val = (color & 0x00FF00) >> 8
-    B_val = (color & 0x0000FF) >> 0
-    # Map color value from 0~255 to 0~100
-    R_val = MAP(R_val, 0, 255, 0, 100)
-    G_val = MAP(G_val, 0, 255, 0, 100)
-    B_val = MAP(B_val, 0, 255, 0, 100)
-
-    #Assign the mapped duty cycle value to the corresponding PWM channel to change the luminance.
-    p_R.ChangeDutyCycle(R_val)
-    p_G.ChangeDutyCycle(G_val)
-    p_B.ChangeDutyCycle(B_val)
-    #print ("color_msg: R_val = %s, G_val = %s,     B_val = %s"%(R_val, G_val, B_val))
+def setcolor(brightness):
+        #print(color)
+        led_red_pwm.value=brightness[0] # 赤LEDを点灯
+        led_green_pwm.value=brightness[1]   # 緑LEDを消灯
+        led_blue_pwm.value=brightness[2]   # 青LEDを点灯
 
 def loop():
     while True:
-        pir_val = GPIO.input(pirPin)
-        if pir_val==GPIO.HIGH:
-            setColor(0xFFFF00)
-        else :
-            setColor(0x0000FF)
+       pir_state = pir_line.get_value()
+       r=random.random()
+       g=random.random()
+       b=random.random()
+
+       if pir_state == 1:
+           setcolor([r,g,b])
+       else:
+           setcolor([0,0,0])
+           
 
 def destroy():
-    p_R.stop()
-    p_G.stop()
-    p_B.stop()
-    GPIO.cleanup()                     # Release resource
+    led_red_pwm.close()
+    led_green_pwm.close()
+    led_blue_pwm.close()
+    pir_line.release()
 
 if __name__ == '__main__':     # Program start from here
     setup()
